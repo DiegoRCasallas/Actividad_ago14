04fa395ab204941d74e86829b9fbcd9cf2ba4e1b
sophia00000000 <laura.hernandez10@usa.edu.co>
2024-08-15 20:15:19 -0500

    Update ejerc2.c

--- parte2/ejerc2.c
+++ parte2/ejerc2.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
-{
-    char contra[8]="12345678";
-    char input[8];
-    printf("Ingrese contraseña");
-    scanf("%s",input);
-    for (int i = 0; i < 8; i++) {
-        if (input[i] != contra[i]) {
-            printf("Contraseña incorrecta\n");
-            return 1;
-        }
+int main() {
+    char contra[9] = "123Abcde"; 
+    char input[9];       
+
+    printf("Ingrese contraseña: ");
+    scanf("%8s", input);        
+
+    if (strcasecmp(contra, input) == 0) {
+        printf("Contraseña correcta\n");
+    } else {
+        printf("Contraseña incorrecta\n");
     }
-    printf("Contraseña correcta\n");
+
     return 0;
 }
